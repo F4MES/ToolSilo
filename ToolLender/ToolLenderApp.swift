aeae25b7bc255dd7f4eebc439b34956449ca9f45
F4MES <tobias.schwartzlose@gmail.com>
2024-12-03 01:39:08 +0100

    Initial Commit

---
+++ ToolLender/ToolLenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToolLenderApp.swift
+//  ToolLender
+//
+//  Created by Tobias Schwartzlose on 03/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToolLenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToolLenderTests/ToolLenderTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToolLenderTests.swift
+//  ToolLenderTests
+//
+//  Created by Tobias Schwartzlose on 03/12/2024.
+//
+
+import Testing
+@testable import ToolLender
+
+struct ToolLenderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
